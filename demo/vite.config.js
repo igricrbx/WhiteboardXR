905a5efeba03c90f9c75e1c448b7c6ea266212f4
igricrbx <igricofficial@gmail.com>
2025-10-25 15:01:49 +0200

    upgrade vite

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   server: {
@@ -9,6 +12,10 @@ export default defineConfig({
       cert: fs.readFileSync(path.resolve(__dirname, '../certs/cert.pem'))
     },
     host: '0.0.0.0', // Allow access from network
-    port: 5173
+    port: 5173,
+    hmr: {
+      protocol: 'wss',
+      clientPort: 5173
+    }
   }
 });
